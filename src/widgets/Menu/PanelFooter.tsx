351d376966218c3a86f1bdc7f06043532d152f51
fullsail-finance <admin@fullsail.finance>
2021-03-08 14:53:02 +0800

    refactor: Upload price link

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -78,7 +78,7 @@ const PanelFooter: React.FC<Props> = ({
     <Container>
       <SocialEntry>
         {cakePriceUsd ? (
-          <PriceLink href="https://pancakeswap.info/token/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" target="_blank">
+          <PriceLink href="https://bscscan.com/token/0x8148b58393f00b4b379cbeb8018d3445e0b636a0" target="_blank">
             <PancakeRoundIcon width="24px" mr="8px" />
             <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
           </PriceLink>
